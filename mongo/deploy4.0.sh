9d30945e444935e0a941444f76afdffd7cd69dd3
Nathan <308719298@qq.com>
2018-10-27 11:33:22 +0800

    add: mongo shell command

---
+++ mongo/deploy4.0.sh
@@ -0,0 +1,23 @@
+username=$1
+password=$2
+
+printHelp() {
+    echo "usage ./mongo/deploy/xxx authuser authpass"
+}
+
+if [[ -z $username || -z $password ]]
+then
+    printHelp
+    exit 1
+fi
+
+mkdir -p /data/mongo/4.0/
+chmod -R 777 /data/mongo
+docker run \
+    --name mongo4.0 \
+    -p :27017 \
+    -d \
+    -v /data/mongo/4.0/data/db:/data/db \
+    -e MONGO_INITDB_ROOT_USERNAME=$username \
+    -e MONGO_INITDB_ROOT_PASSWORD=$password \
+    mongo:4.0
