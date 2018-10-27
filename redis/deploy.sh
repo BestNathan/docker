9decae3d9522cefd3a1f872ac0b0630944a1bc3d
Nathan <308719298@qq.com>
2018-10-27 12:08:23 +0800

    add: redis shell command

---
+++ redis/deploy.sh
@@ -0,0 +1,23 @@
+password=$1
+
+printHelp() {
+    echo "usage ./redis/deploy.sh requirepass"
+}
+
+if [[ -z $password ]]
+then
+    printHelp
+    exit 1
+fi
+
+mkdir -p /data/redis/
+chmod -R 777 /data/redis
+docker run \
+    --name redis \
+    -p :6379 \
+    -d \
+    -v /data/redis/data:/data \
+    redis \
+        redis-server \
+            --appendonly yes \
+            --requirepass $password \
